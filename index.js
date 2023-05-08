1005c62b568f0e0c291859162e4a83e7057b7ed7
Kartikay <kartiksrivastava19@gmail.com>
2023-05-08 16:14:30 +0530

    complete exercises3.16 of Phonebook database from part3 step4 Add separate error handling middleware in the application

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ morgan.token("body", (request) => {
 
 const app = express();
 app.use(cors())
-app.use(express.json());
 app.use(express.static('build'))
+app.use(express.json());
 
 const assignBodyJson = (request, response, next) => {
   request.body = JSON.stringify(request.body);
@@ -23,10 +23,10 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-app.get("/api/persons", (request, response) => {
+app.get("/api/persons", (request, response, next) => {
   Person.find({}).then(result => {
     response.json(result)
-  })
+  }).catch(error => next(error))
 });
 
 app.use((request, response, next) => {
@@ -35,10 +35,12 @@ app.use((request, response, next) => {
 });
 
 app.get("/info", (request, response) => {
-  const entries = persons.length;
   const requestTime = new Date(request.requestTime);
-  let responseText = `<div>Phonebook has info for ${entries} people</div><br><div>${requestTime}</div>`;
-  response.send(responseText);
+  Person.countDocuments({}).then(result => {
+    console.log(result);
+    let responseText = `<div>Phonebook has info for ${result} people</div><br><div>${requestTime}</div>`;
+    response.send(responseText);
+  }).catch(error => next(error))
 });
 
 app.get("/api/persons/:id", (request, response) => {
@@ -55,7 +57,7 @@ app.get("/api/persons/:id", (request, response) => {
 app.delete("/api/persons/:id", (request, response) => {
   Person.findByIdAndDelete(request.params.id).then(result => {
     response.status(204).end()
-  })
+  }).catch(error => next(error))
 });
 
 app.post("/api/persons", (request, response) => {
@@ -78,8 +80,23 @@ app.post("/api/persons", (request, response) => {
 
   contact.save().then(savedContact => {
     response.json(savedContact)
-  })
+  }).catch(error => next(error))
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({error: 'unknown endpoint'})
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(404).send({ error: 'malformatted id' })
+  }
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
